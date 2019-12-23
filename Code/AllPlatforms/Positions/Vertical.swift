6fdcccf75346ebde9940ed34cb471744b86bc782
flowtoolz <contact@flowtoolz.com>
2019-12-23 15:14:50 +0100

    Finish decomposing code originally imported from playground

--- Code/AllPlatforms/Positions/Vertical.swift
+++ Code/AllPlatforms/Positions/Vertical.swift
@@ -7,6 +7,38 @@ import UIKit
 public extension LayoutItem
 {
     @discardableResult
+    func constrain(above item: LayoutItem?, maxGap: CGFloat) -> [NSLayoutConstraint?] {
+        [ constrain(above: item, minGap: 0),
+          constrain(.bottom, to: item?.top(offset: -maxGap).min) ]
+    }
+    
+    @discardableResult
+    func constrain(below item: LayoutItem?, maxGap: CGFloat) -> [NSLayoutConstraint?] {
+        [ constrain(below: item, minGap: 0),
+          constrain(.top, to: item?.bottom(offset: maxGap).max) ]
+    }
+    
+    @discardableResult
+    func constrain(above item: LayoutItem?, minGap: CGFloat) -> NSLayoutConstraint? {
+        constrain(.bottom, to: item?.top(offset: -minGap).max)
+    }
+    
+    @discardableResult
+    func constrain(below item: LayoutItem?, minGap: CGFloat) -> NSLayoutConstraint? {
+        constrain(.top, to: item?.bottom(offset: minGap).min)
+    }
+    
+    @discardableResult
+    func constrain(above item: LayoutItem?, gap: CGFloat = 0) -> NSLayoutConstraint? {
+        constrain(.bottom, to: item?.top(offset: -gap))
+    }
+    
+    @discardableResult
+    func constrain(below item: LayoutItem?, gap: CGFloat = 0) -> NSLayoutConstraint? {
+        constrain(.top, to: item?.bottom(offset: gap))
+    }
+    
+    @discardableResult
     func constrain(to target: VerticalTarget?) -> NSLayoutConstraint? {
         guard let target = target else { return nil }
         return constrain(target.anchor.position, to: target)
