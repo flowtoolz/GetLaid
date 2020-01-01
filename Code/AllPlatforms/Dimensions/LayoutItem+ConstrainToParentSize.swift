f3661556593a41dad83aab31143156d68cbf74a2
flowtoolz <contact@flowtoolz.com>
2020-01-01 14:19:50 +0100

    Add convenience funcs for constraining size to parent

---
+++ Code/AllPlatforms/Dimensions/LayoutItem+ConstrainToParentSize.swift
@@ -0,0 +1,44 @@
+#if os(OSX)
+import AppKit
+#else
+import UIKit
+#endif
+
+public extension LayoutItem
+{
+    @discardableResult
+    func constrainToParentSize(relation: Relation = .exact) -> [NSLayoutConstraint]
+    {
+        constrain(to: parent?.size.with(relation))
+    }
+    
+    @discardableResult
+    func constrainToParentSize(at factor: CGFloat) -> [NSLayoutConstraint]
+    {
+        constrain(to: parent?.size.at(factor))
+    }
+    
+    @discardableResult
+    func constrainToParentHeight(relation: Relation = .exact) -> NSLayoutConstraint?
+    {
+        height.constrain(to: parent?.height, relation: relation)
+    }
+    
+    @discardableResult
+    func constrainToParentWidth(relation: Relation = .exact) -> NSLayoutConstraint?
+    {
+        width.constrain(to: parent?.width, relation: relation)
+    }
+    
+    @discardableResult
+    func constrainToParentHeight(at factor: CGFloat) -> [NSLayoutConstraint]
+    {
+        constrain(to: parent?.height.at(factor))
+    }
+    
+    @discardableResult
+    func constrainToParentWidth(at factor: CGFloat) -> [NSLayoutConstraint]
+    {
+        constrain(to: parent?.width.at(factor))
+    }
+}
