09510113e66eea62381e84e34d6ec89a0f19d503
flowtoolz <contact@flowtoolz.com>
2020-01-02 01:05:32 +0100

    Add parentSafeArea property to UILayoutGuide

--- Code/UIKit/UIView+SafeArea.swift
+++ Code/UIKit/UIView+SafeArea.swift
@@ -6,3 +6,9 @@ public extension UIView
     var parentSafeArea: UILayoutGuide? { superview?.safeArea }
     var safeArea: UILayoutGuide { safeAreaLayoutGuide }
 }
+
+@available(iOS 11.0, tvOS 11.0, *)
+public extension UILayoutGuide
+{
+    var parentSafeArea: UILayoutGuide? { owningView?.safeArea }
+}
