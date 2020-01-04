a2fe3c261e127a7cfe5bafbd9aced836f8818da3
flowtoolz <contact@flowtoolz.com>
2020-01-04 23:16:25 +0100

    Add iOS development/example project with deployment target of iOS 12.0

---
+++ Example Projects/GetLaidDevIOS/GetLaidDevIOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GetLaidDevIOS
+//
+//  Created by seb on 04.01.20.
+//  Copyright © 2020 flowtoolz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        
+    }
+
+
+}
+
