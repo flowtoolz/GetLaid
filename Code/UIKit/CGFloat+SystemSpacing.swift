801e63576c8a1047b840cf3854290a8ded5a8a09
flowtoolz <contact@flowtoolz.com>
2020-01-05 00:46:09 +0100

    Make system spacing constant global and add 2 more specific ones

--- Code/UIKit/CGFloat+SystemSpacing.swift
+++ Code/UIKit/CGFloat+SystemSpacing.swift
@@ -1,14 +1,26 @@
 import UIKit
 
 @available(iOS 11.0, tvOS 11.0, *)
-public extension CGFloat
+public var systemSpacing: CGFloat { systemSiblingSpacing }
+
+@available(iOS 11.0, tvOS 11.0, *)
+public let systemSiblingSpacing: CGFloat =
 {
-    static let systemSpacing: CGFloat =
-    {
-        let view = UIView()
-        let subview = UIView()
-        view.addForAutoLayout(subview)
-        return subview.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor,
-                                             multiplier: 1).constant
-    }()
-}
+    let view = UIView()
+    let subview = UIView()
+    let subview2 = UIView()
+    view.addForAutoLayout(subview)
+    view.addForAutoLayout(subview2)
+    return subview2.leftAnchor.constraint(equalToSystemSpacingAfter: subview.rightAnchor,
+                                          multiplier: 1).constant
+}()
+
+@available(iOS 11.0, tvOS 11.0, *)
+public let systemParentSpacing: CGFloat =
+{
+    let view = UIView()
+    let subview = UIView()
+    view.addForAutoLayout(subview)
+    return subview.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor,
+                                         multiplier: 1).constant
+}()
