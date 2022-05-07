a4ee1ab503cb52122fe30694d6317a6717f8e23e
Sebastian Fichtner <contact@flowtoolz.com>
2022-05-07 12:55:16 +0100

    Document precedence issue

--- Sources/GetLaid/AllPlatforms/Core API/ConstrainOperatorPrecedence.swift
+++ Sources/GetLaid/AllPlatforms/Core API/ConstrainOperatorPrecedence.swift
@@ -1,1 +1,5 @@
+/** TODO: `infix operator >>: AssignmentPrecedence` does not work anywhere. since we heavily overload the operator, trying to declare precedence creates ambiguity compile errors ...
+ 
+ example use case: brackets around the multiplication should be unnecessary: `(view.height >> (defaultHeight * 1.3)).priority = .init(999)`
+*/
 //infix operator >>: AssignmentPrecedence
